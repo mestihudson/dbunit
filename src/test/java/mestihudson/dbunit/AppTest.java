aeb208752c8ca57f4197d3591b7485f30811d564
mestihudson <mestihudson@gmail.com>
2015-04-13 17:45:04 -0300

    evolução do framework

--- src/test/java/mestihudson/dbunit/AppTest.java
+++ src/test/java/mestihudson/dbunit/AppTest.java
@@ -11,22 +11,7 @@ import java.io.FileOutputStream;
 import java.sql.DriverManager;
 
 public class AppTest {
-  @Test
-  @Ignore
-  public void extract_from_db() throws Throwable {
-    Class.forName("org.h2.Driver");
-
-    IDatabaseConnection connection = new DatabaseConnection(DriverManager.getConnection("jdbc:h2:tcp://localhost/~/Downloads/maven/db/unit", "sa", ""));
-    // QueryDataSet qds = new QueryDataSet(connection);
-    // qds.addTable("recipe", "SELECT * FROM recipe where is_deleted != 'Y'");
-    // qds.addTable("recipe_ext_xref");
-    // FlatXmlDataSet.write(qds, new FileOutputStream("partial-dataset.xml"));
-
-    IDataSet fds = connection.createDataSet();
-    FlatXmlDataSet.write(fds, new FileOutputStream("target/full-dataset.xml"));
-  }
-
   @Test public void db_load() throws Throwable {
-    DB.load("criar-administrador-sistema", 2, "Meu Primeiro Nome");
+    DB.fixture("criar-administrador-sistema", 2, "Meu Primeiro Nome", "Qualquer coisa que tenha ' ou até mais como ''");
   }
 }
--- src/test/java/mestihudson/dbunit/DB.java
+++ src/test/java/mestihudson/dbunit/DB.java
@@ -1,29 +1,67 @@
 package mestihudson.dbunit;
 
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.text.MessageFormat;
-import java.nio.charset.Charset;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import org.apache.commons.io.FileUtils;
 
 public class DB {
-  public static void load(String name, Object... parameters) throws Throwable {
-    fill(name, parameters);
-    execute(name);
+  public static void fixture(String name, Object... parameters) throws Throwable {
+    String content = load(fix(name), parameters);
+    execute(content);
   }
 
-  private static void fill(String name, Object... parameters) throws Throwable {
-    File file = file(name);
-    String content = FileUtils.readFileToString(file, Charset.defaultCharset());
+  private static String load(File file, Object... parameters) throws Throwable {
+    String content = FileUtils.readFileToString(file);
     content = MessageFormat.format(content, parameters);
-    FileUtils.writeStringToFile(file, content, Charset.defaultCharset());
+    return compile(content);
   }
 
-  private static void execute(String name) throws Throwable {
-    System.out.println(FileUtils.readFileToString(file(name)));
+  private static String compile(String content) throws Throwable {
+    List<String> lines = Arrays.asList(content.split("\\n"));
+    String result = "";
+    String pattern = "^\\!(([\\w][\\w\\-]*)(\\/[\\w][\\w\\-]*)*)\\:(.+)$";
+    for(String line : lines){
+      Matcher matcher = Pattern.compile(pattern).matcher(line);
+      if(matcher.find()){
+        result += indirection(matcher.group(1), matcher.group(4));
+      }else{
+        result += line + "\n";
+      }
+    }
+    return result;
+  }
+
+  private static String indirection(String name, String parameters) throws Throwable {
+    String pattern = "([^,]+)";
+    Matcher matcher = Pattern.compile(pattern).matcher(parameters);
+    List params = new ArrayList();
+    while(matcher.find()){
+      params.add(matcher.group(1).trim());
+    }
+    return load(fix(name), params.toArray());
+  }
+
+  private static void execute(String content) throws Throwable {
+    System.err.println(content);
+  }
+
+  private static File fix(String name) throws Throwable {
+    return file(name + ".fixture");
   }
 
   private static File file(String name) throws Throwable {
-    return new File(DB.class.getClassLoader().getResource(name).getPath());
+    File file;
+    try{
+      file = new File(DB.class.getClassLoader().getResource(name).getPath());
+    }catch(Throwable t) {
+      throw new Exception("Arquivo [" + name + "] não localizado.");
+    }
+    return file;
   }
 }
