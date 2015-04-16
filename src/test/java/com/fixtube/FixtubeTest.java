ddda30a75a048f01dfecc8d5f8bc53da2ac4c373
mestihudson <mestihudson@gmail.com>
2015-04-16 18:08:45 -0300

    refatoração de teste

--- src/test/java/com/fixtube/FixtubeTest.java
+++ src/test/java/com/fixtube/FixtubeTest.java
@@ -11,63 +11,25 @@ public class FixtubeTest {
     Fixtube.fixture("criar-administrador-sistema", "84571713304", "Hudson Leite");
   }
 
+  private void assertParserParameters(String content, int size, String... values) {
+    ParseParameters parser = new ParseParameters(content);
+    assertThat(parser.getParameters().size(), equalTo(size));
+    for(int i = 0, l = parser.getParameters().size(); i < l; i++){
+      assertThat(parser.getParameters().get(i).toString(), equalTo(values[i]));
+    }
+  }
+
   @Test public void parse_params() throws Throwable {
-    ParseParameters parser = new ParseParameters("1");
-    assertThat(parser.getParameters().size(), equalTo(1));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-
-    parser = new ParseParameters("1, 2");
-    assertThat(parser.getParameters().size(), equalTo(2));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2"));
-
-    parser = new ParseParameters("1, {2}");
-    assertThat(parser.getParameters().size(), equalTo(2));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("{2}"));
-
-    parser = new ParseParameters("1, \"2\"");
-    assertThat(parser.getParameters().size(), equalTo(2));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2"));
-
-    parser = new ParseParameters("1, 	\"2\"");
-    assertThat(parser.getParameters().size(), equalTo(2));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2"));
-
-    parser = new ParseParameters("1,  \"2\"    ");
-    assertThat(parser.getParameters().size(), equalTo(2));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2"));
-
-    parser = new ParseParameters("1, 	\"2\"         ");
-    assertThat(parser.getParameters().size(), equalTo(2));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2"));
-
-    parser = new ParseParameters("1, \"2, 3 ou 4\", 5");
-    assertThat(parser.getParameters().size(), equalTo(3));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2, 3 ou 4"));
-    assertThat(parser.getParameters().get(2).toString(), equalTo("5"));
-
-    parser = new ParseParameters("1, \"2, 3 ou 4\"      , 5");
-    assertThat(parser.getParameters().size(), equalTo(3));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2, 3 ou 4"));
-    assertThat(parser.getParameters().get(2).toString(), equalTo("5"));
-
-    parser = new ParseParameters("1, 2\\, 3 ou 4, 5");
-    assertThat(parser.getParameters().size(), equalTo(3));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("2, 3 ou 4"));
-    assertThat(parser.getParameters().get(2).toString(), equalTo("5"));
-
-    parser = new ParseParameters("1, \\\"2\\, 3 ou 4\\\", 5");
-    assertThat(parser.getParameters().size(), equalTo(3));
-    assertThat(parser.getParameters().get(0).toString(), equalTo("1"));
-    assertThat(parser.getParameters().get(1).toString(), equalTo("\"2, 3 ou 4\""));
-    assertThat(parser.getParameters().get(2).toString(), equalTo("5"));
+    assertParserParameters("1", 1, "1");
+    assertParserParameters("1, 2", 2, "1", "2");
+    assertParserParameters("1, {2}", 2, "1", "{2}");
+    assertParserParameters("1, \"2\"", 2, "1", "2");
+    assertParserParameters("1,  \"2\"", 2, "1", "2");
+    assertParserParameters("1,  \"2\"    ", 2, "1", "2");
+    assertParserParameters("1,  \"2\"         ", 2, "1", "2");
+    assertParserParameters("1, \"2, 3 ou 4\", 5", 3, "1", "2, 3 ou 4", "5");
+    assertParserParameters("1, \"2, 3 ou 4\"      , 5", 3, "1", "2, 3 ou 4", "5");
+    assertParserParameters("1, 2\\, 3 ou 4, 5", 3, "1", "2, 3 ou 4", "5");
+    assertParserParameters("1, \\\"2\\, 3 ou 4\\\", 5", 3, "1", "\"2, 3 ou 4\"", "5");
   }
 }
